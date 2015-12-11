a4c378d50a860cb98e57b9ced33364ec3018c622
xadro <mothedude@gmail.com>
2015-12-11 23:01:57 +0100

    almost fixed gfx

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,33 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 25832587264
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64) #verified
 ARGET_USERIMAGES_USE_EXT4 := true
 
+#Graphics
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+BOARD_EGL_CFG := device/moto/clark/egl.cfg
+
+USE_OPENGL_RENDERER := true
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+TARGET_USES_ION := true
+
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+HAVE_ADRENO_SOURCE:= false
+
+OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
+
+
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_MISC_PARTITION := true
@@ -69,18 +96,19 @@ BOARD_RECOVERY_SWIPE := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_USES_MMCUTILS := true
 TARGET_PREBUILT_KERNEL := device/motorola/kinzie/recovery/kernel
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_FSTAB = device/motorola/kinzie/twrp.fstab
 
 # TWRP Build Flags
 DEVICE_RESOLUTION := 1440x2560
-TW_THEME := portrait_hdpi
-TW_HAS_DOWNLOAD_MODE := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
+TW_INCLUDE_L_CRYPTO := true
+TW_NEW_ION_HEAP := true
 TW_INCLUDE_CRYPTO := true
-TW_NO_EXFAT_FUSE := true
-TW_NO_SCREEN_BLANK := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
 
 # Vendor Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
