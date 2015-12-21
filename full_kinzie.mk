609ea88702fbf1ac778e2406c286262a1ca12c54
xadro <mothedude@gmail.com>
2015-12-21 18:14:31 +0100

    omnifi tree part 7

--- full_kinzie.mk
+++ full_kinzie.mk
@@ -17,27 +17,26 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+#$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
+#inherit vendor tree for kinzie if avail
 $(call inherit-product-if-exists, vendor/motorola/kinzie/kinzie-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
+#DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
 
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/motorola/kinzie/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	LOCAL_KERNEL := device/motorola/kinzie/kernel
+#else
+#	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+$(call inherit-product, device/motorola/kinzie/device.mk)
 
-$(call inherit-product, build/target/product/full.mk)
-
-#PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := cm_kinzie-userdebug
+PRODUCT_NAME := full-kinzie
 PRODUCT_DEVICE := kinzie
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
