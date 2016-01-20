a2ac92e2a3b900e51698238262b7b952fa27b064
xadro <mothedude@gmail.com>
2016-01-20 13:22:30 +0100

    Update omni-kinzie.mk

--- omni-kinzie.mk
+++ omni-kinzie.mk
@@ -3,6 +3,9 @@
 #build for gsm
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/motorola/kinzie/full_kinzie.mk)
 # Release name
