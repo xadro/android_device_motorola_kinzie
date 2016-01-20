65b4f050aaf458da127843e40736a44c0769f031
xadro <mothedude@gmail.com>
2016-01-20 13:31:03 +0100

    Update omni-kinzie.mk

--- omni-kinzie.mk
+++ omni-kinzie.mk
@@ -13,6 +13,8 @@ PRODUCT_RELEASE_NAME := kinzie
 
 # Inherit some common DU stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, device/motorola/kinzie/device.mk)
+
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kinzie
